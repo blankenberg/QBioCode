2888c8b0ae42a77bab956c1e0bc5b6fa606c730e
Filippo Utro <futro@us.ibm.com>
2025-05-28 14:14:21 -0400

    setup py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description. It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# Function to read the requirements from requirements.txt
+def read_requirements():
+    with open('requirements.txt') as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+setup(
+    name="qml4omics",  # Replace with your project's name
+    version="0.0.1",         # Start with a small version number and increment as needed
+    author="Bryan Raubenolt, Aritra Bose, Kahn Rhrissorrakrai, Filippo Utro, Akhil Mohan, Daniel Blankenberg, Laxmi Parida",      # Replace with your name
+    author_email="your.email@example.com", # Replace with your email
+    description=("A short description of your project."),
+    license="Apache 2.0",           # Choose an appropriate license (e.g., MIT, Apache 2.0, GPLv3)
+    keywords="machine learning, omics, qml, oracle, profiler", # Keywords for your project
+    url="https://github.com/IBM/qml4omics/tree/", # Replace with your project's URL (e.g., GitHub repo)
+    packages=find_packages(), # Automatically find all packages in your project
+    long_description=read('README.md') if os.path.exists('README.md') else '', # Reads from README.md if present
+    long_description_content_type='text/markdown', # Set this if your README is in Markdown
+    classifiers=[
+        "Development Status :: 3 - Alpha", # Choose appropriate development status
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache 2.0",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    
+        "Operating System :: OS Independent",
+    ],
+    install_requires=read_requirements(), # This pulls dependencies from requirements.txt
+    python_requires='>=3.7', # Specify the minimum Python version required
+    # If you have non-code files that need to be included in your package,
+    # uncomment and configure package_data or include_package_data
+    # package_data={
+    #     'your_package_name': ['data/*.txt', 'templates/*.html'],
+    # },
+    # include_package_data=True, # Set to True to include data files specified in MANIFEST.in
+    # entry_points={
+    #     'console_scripts': [
+    #         'my_project_command=my_project_name.cli:main', # Example: if you have a command-line script
+    #     ],
+    # },
+)
