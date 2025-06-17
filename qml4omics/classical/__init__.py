39f78b57042c1df75176e097683337996c41fd4e
Filippo Utro <futro@us.ibm.com>
2025-06-17 17:03:52 -0400

    update

--- qml4omics/classical/__init__.py
+++ qml4omics/classical/__init__.py
@@ -1,1 +1,7 @@
 
+from .supervised.compute_dt import compute_dt
+from .supervised.compute_lr import compute_lr
+from .supervised.compute_mlp import  compute_mlp
+from .supervised.compute_nb  import compute_nb
+from .supervised.compute_rf  import compute_rf
+from .supervised.compute_svc  import compute_svc
--- qml4omics/classical/supervised/__init__.py
+++ qml4omics/classical/supervised/__init__.py
@@ -1,1 +1,6 @@
-
+from .compute_dt import compute_dt
+from .compute_lr import compute_lr
+from .compute_mlp import  compute_mlp
+from .compute_nb  import compute_nb
+from .compute_rf  import compute_rf
+from .compute_svc  import compute_svc
