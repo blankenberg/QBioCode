1820e6640824044516287ceaddbde2302c45eac1
Kahn Rhrissorrakrai <krhriss@us.ibm.com>
2026-03-23 12:03:56 -0400

    clean up ensemble code

--- tutorial/QEnsemble/modeling_random_unitary.py
+++ tutorial/QEnsemble/modeling_random_unitary.py
@@ -8,23 +8,13 @@ to ensemble construction compared to fixed swap patterns.
 
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit.compiler import transpile
-from qiskit.visualization import *
 from qiskit_aer import AerSimulator
-from qiskit_aer.primitives import SamplerV2
-from Utils import *
 import Utils
-from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-from qiskit.transpiler import PassManager, InstructionProperties
-from qiskit.transpiler.passes.scheduling import (
-    ALAPScheduleAnalysis,
-    PadDynamicalDecoupling,
-)
 from qiskit_aer import Aer
-from qiskit.transpiler.passes import BasisTranslator
-from qiskit.circuit.equivalence_library import SessionEquivalenceLibrary as sel
-from qiskit.circuit.library import ZFeatureMap, ZZFeatureMap, RXGate, RYGate, RZGate, UnitaryGate
+from qiskit.circuit.library import UnitaryGate
 import scipy.stats
-
+import numpy as np
+import pandas as pd
 
 def run_ensemble(d, n_train, seed, n_swap, X_train, X_test, y_train, y_test, mode="balanced", n_shots=8192):
     """
@@ -83,94 +73,6 @@ def run_ensemble(d, n_train, seed, n_swap, X_train, X_test, y_train, y_test, mod
     else:
         return pd.DataFrame()
 
-def cos_classifier_OLD(train, test, label_train, printing=False):
-    """
-    Legacy cosine classifier for single qubit data.
-    
-    Parameters
-    ----------
-    train : array-like
-        Training data point (normalized vector)
-    test : array-like
-        Test data point (normalized vector)
-    label_train : array-like
-        Training label (normalized vector)
-    printing : bool, optional
-        If True, print the quantum circuit (default: False)
-    
-    Returns
-    -------
-    QuantumCircuit
-        Quantum circuit implementing the cosine classifier
-    """
-    c = ClassicalRegister(1, 'c')
-    x_train = QuantumRegister(1, 'x^{(i)}')
-    x_test = QuantumRegister(1, 'x^{(test)}')
-    y_train = QuantumRegister(1, 'y^{(i)}')
-    y_test = QuantumRegister(1, 'y^{(test)}')
-    qc = QuantumCircuit(x_train, x_test, y_train, y_test, c)
-    qc.initialize(train, [x_train[0]])
-    qc.initialize(test, [x_test[0]])
-    qc.initialize(label_train, [y_train[0]])
-    qc.barrier()
-    qc.h(y_test)
-    qc.cswap(y_test, x_train, x_test)
-    qc.h(y_test)
-    qc.barrier()
-    qc.cx(y_train, y_test)
-    qc.measure(y_test, c)
-    if printing:
-        print(qc)
-    return qc
-
-
-def cos_classifier(train, test, label_train, printing=False):
-    """
-    Cosine classifier for multi-qubit data.
-    
-    Parameters
-    ----------
-    train : array-like
-        Training data point (normalized vector)
-    test : array-like
-        Test data point (normalized vector)
-    label_train : array-like
-        Training label (normalized vector)
-    printing : bool, optional
-        If True, print the quantum circuit (default: False)
-    
-    Returns
-    -------
-    QuantumCircuit
-        Quantum circuit implementing the cosine classifier
-    """
-    n_obs = len(train)
-    qubits_per = int(np.log2(len(train)))
-    n_obs_qubits = qubits_per * n_obs
-    n_test = len(test)
-    n_test_qubits = qubits_per * n_test
-
-    c = ClassicalRegister(1, 'c')
-    x_train = QuantumRegister(qubits_per, 'x_{b}')
-    x_test = QuantumRegister(qubits_per, 'x^{(test)}')
-    y_train = QuantumRegister(1, 'y_{b}')
-    y_test = QuantumRegister(1, 'y^{(test)}')
-    qc = QuantumCircuit(x_train, x_test, y_train, y_test, c)
-    
-    qc.initialize(train, [x_train])
-    qc.initialize(test, [x_test])
-    qc.initialize(label_train, [y_train])
-    qc.barrier()
-    qc.h(y_test)
-    qc.cswap(y_test, x_train, x_test)
-    qc.h(y_test)
-    qc.barrier()
-    qc.cx(y_train, y_test)
-    qc.measure(y_test, c)
-    if printing:
-        print(qc)
-    return qc
-
 
 def state_prep(x):
     """
@@ -187,7 +89,7 @@ def state_prep(x):
         Unitary matrix representing the state preparation operation
     """
     backend = AerSimulator(method="unitary")
-    x = normalize_custom(x)
+    x = Utils.normalize_custom(x)
     qreg = QuantumRegister(1)
     qc = QuantumCircuit(qreg)
     qc.prepare_state(x, [qreg])
@@ -197,56 +99,6 @@ def state_prep(x):
     U = result.get_unitary(qc)
     return U
 
-
-def quantum_cosine_classifier(train, test, label_train):
-    """
-    Quantum cosine classifier using unitary state preparation.
-    
-    Parameters
-    ----------
-    train : array-like
-        Training data points (2D array)
-    test : array-like
-        Test data points (2D array)
-    label_train : array-like
-        Training labels
-    
-    Returns
-    -------
-    QuantumCircuit
-        Quantum circuit implementing the classifier with unitary gates
-    """
-    n_obs = len(train)
-    qubits_per = int(np.log2(train.shape[1]))
-    n_obs_qubits = qubits_per * n_obs
-    n_test = len(test)
-    n_test_qubits = qubits_per * n_test
-
-    c = ClassicalRegister(n_test, 'c')
-    x_train = QuantumRegister(n_obs_qubits, 'x_{b}')
-    x_test = QuantumRegister(n_test_qubits, 'x^{(test)}')
-    y_train = QuantumRegister(n_obs, 'y_{b}')
-    y_test = QuantumRegister(n_test, 'y^{(test)}')
-    qc = QuantumCircuit(x_train, x_test, y_train, y_test, c)
-    
-    S1 = state_prep(train)
-    qc.unitary(S1, [0], label='$S_{x}$')
-
-    S2 = state_prep(test)
-    qc.unitary(S2, [1], label='$S_{x}$')
-    
-    S3 = state_prep(label_train)
-    qc.unitary(S3, [2], label='$S_{y}$')
-
-    qc.barrier()
-    qc.h(y_test)
-    qc.cswap(y_test, x_train, x_test)
-    qc.h(y_test)
-    qc.barrier()
-    qc.cx(y_train, y_test)
-    qc.measure(y_test, c)
-    return qc
- 
 
 
 def ensemble(X_data, Y_data, x_test, n_swap=1, d=2, mode="balanced", barriers=False):
@@ -379,212 +231,6 @@ def ensemble(X_data, Y_data, x_test, n_swap=1, d=2, mode="balanced", barriers=Fa
     return qc
 
 
-def ensemble_fixed_U(X_data, Y_data, x_test, d=2):
-    """
-    Quantum ensemble with fixed unitary operations.
-    
-    Parameters
-    ----------
-    X_data : array-like
-        Training data points
-    Y_data : array-like
-        Training labels
-    x_test : array-like
-        Test data point
-    d : int, optional
-        Number of control qubits (default: 2)
-    
-    Returns
-    -------
-    QuantumCircuit
-        Quantum circuit with fixed unitary ensemble operations
-    """
-    n_obs = len(X_data)
-
-    if n_obs != len(Y_data):
-        return print('Error: in the input size')
-
-    n_reg = d + 2 * n_obs + 1  # total number of registers
-
-    control = QuantumRegister(d, 'd')
-    data = QuantumRegister(n_obs, 'x')
-    labels = QuantumRegister(n_obs, 'y')
-    data_test = QuantumRegister(1, 'x^{(test)}')
-    label_test = QuantumRegister(1, 'y^{(test)}')
-    c = ClassicalRegister(1, 'c')
-
-    qc = QuantumCircuit(control, data, labels, data_test, label_test, c)
-    
-    
-    for index in range(n_obs):
-
-        Sx = state_prep(X_data[index])
-        qc.unitary(Sx, [2+index], label='$S_{x}$')
-        
-        Sy = state_prep(Y_data[index])
-        qc.unitary(Sy, [6+index], label='$S_{y}$')
-
-
-    for i in range(d):
-        qc.h(control[i])
-
-    U1 = [0, 2]  # np.random.choice(range(4), 2, replace=False)
-    U2 = [1, 3]  # np.random.choice(range(4), 2, replace=False)
-    U4 = [2,3]  # np.random.choice(range(4), 2, replace=False)
-
-    qc.barrier()
-
-    # U1
-    qc.cswap(control[0], data[int(U1[0])], data[int(U1[1])])
-    qc.cswap(control[0], labels[int(U1[0])], labels[int(U1[1])])
-
-    qc.x(control[0])
-
-    # U2
-    qc.cswap(control[0], data[int(U2[0])], data[int(U2[1])])
-    qc.cswap(control[0], labels[int(U2[0])], labels[int(U2[1])])
-
-    qc.barrier()
-
-
-
-    qc.x(control[1])
-
-    # U4
-    qc.cswap(control[1], data[int(U4[0])], data[int(U4[1])])
-    qc.cswap(control[1], labels[int(U4[0])], labels[int(U4[1])])
-
-    qc.barrier()
-    Sx = state_prep(x_test)
-    qc.unitary(Sx, [10], label='$S_{x}$')
-
-    # qc.barrier()
-
-    ix_cls = 3
-    qc.barrier()
-    qc.h(label_test[0])
-    qc.cswap(label_test[0], data[ix_cls], data_test[0])
-    qc.h(label_test[0])
-    qc.cx(labels[ix_cls], label_test[0])
-    qc.measure(label_test[0], c)
-    return qc
-
-
-def ensemble_random_swap(X_data, Y_data, x_test, d=2):
-    """
-    Quantum ensemble using ancilla qubits for random swap operations.
-    
-    Parameters
-    ----------
-    X_data : array-like
-        Training data points
-    Y_data : array-like
-        Training labels
-    x_test : array-like
-        Test data point
-    d : int, optional
-        Number of control qubits (default: 2)
-    
-    Returns
-    -------
-    QuantumCircuit
-        Quantum circuit with ancilla-based random swap ensemble
-    """
-    n_obs = len(X_data)
-    n_reg = d + 4 * n_obs + 1
-
-    control = QuantumRegister(d, 'control')
-    data = QuantumRegister(n_obs, 'x')
-    labels = QuantumRegister(n_obs, 'y')
-    ancilla_x = QuantumRegister(n_obs, 'ancilla_x')
-    ancilla_y = QuantumRegister(n_obs, 'ancilla_y')
-    ancilla_test = QuantumRegister(2, 'ancilla_test')
-    data_test = QuantumRegister(1, 'x test')
-    label_test = QuantumRegister(1, 'y test')
-    c = ClassicalRegister(1)
-
-    qc = QuantumCircuit(control, data, labels, data_test, label_test, ancilla_x, ancilla_y, ancilla_test, c)
-
-    for index in range(n_obs):
-        qc.initialize(X_data[index], [data[index]])
-        qc.initialize(Y_data[index], [labels[index]])
-
-
-    qc.initialize(x_test, [data_test[0]])
-
-
-    for i in range(d):
-        qc.h(control[i])
-
-    U1 = np.random.choice(range(n_obs), 2, replace=False)
-    U2 = np.random.choice(range(n_obs), 2, replace=False)
-    U3 = np.random.choice(range(n_obs), 2, replace=False)
-    U4 = np.random.choice(range(n_obs), 2, replace=False)
-
-    qc.barrier()
-
-    for i in range(n_obs):
-        qc.cswap(control[0], data[i], ancilla_x[i])
-        qc.cswap(control[0], labels[i], ancilla_y[i])
-    qc.cswap(control[0], data_test[0], ancilla_test[0])
-    qc.cswap(control[0], label_test[0], ancilla_test[1])
-
-    qc.barrier()
-
-    # U1
-    qc.swap(data[int(U1[0])], data[int(U1[1])])
-    qc.swap(labels[int(U1[0])], labels[int(U1[1])])
-
-
-    # U2
-    qc.swap(ancilla_x[int(U2[0])], ancilla_x[int(U2[1])])
-    qc.swap(ancilla_y[int(U2[0])], ancilla_x[int(U2[1])])
-
-    qc.barrier()
-
-    for i in range(n_obs):
-        qc.cswap(control[0], data[i], ancilla_x[i])
-        qc.cswap(control[0], labels[i], ancilla_y[i])
-    qc.cswap(control[0], data_test[0], ancilla_test[0])
-    qc.cswap(control[0], label_test[0], ancilla_test[1])
-
-    qc.barrier()
-
-    for i in range(n_obs):
-        qc.cswap(control[1], data[i], ancilla_x[i])
-        qc.cswap(control[1], labels[i], ancilla_y[i])
-    qc.cswap(control[1], data_test[0], ancilla_test[0])
-    qc.cswap(control[1], label_test[0], ancilla_test[1])
-
-    qc.barrier()
-
-    # U3
-    qc.swap(data[int(U3[0])], data[int(U3[1])])
-    qc.swap(labels[int(U3[0])], labels[int(U3[1])])
-
-    # U4
-    qc.swap(ancilla_x[int(U4[0])], ancilla_x[int(U4[1])])
-    qc.swap(ancilla_y[int(U4[0])], ancilla_y[int(U4[1])])
-
-    qc.barrier()
-
-    for i in range(n_obs):
-        qc.cswap(control[1], data[i], ancilla_x[i])
-        qc.cswap(control[1], labels[i], ancilla_y[i])
-    qc.cswap(control[1], data_test[0], ancilla_test[0])
-    qc.cswap(control[1], label_test[0], ancilla_test[1])
-
-    qc.barrier()
-
-    # C
-    ix_cls = 3
-
-    qc.h(label_test[0])
-    qc.cswap(label_test[0], data[ix_cls], data_test[0])
-    qc.h(label_test[0])
-    qc.cx(labels[ix_cls], label_test[0])
-    qc.measure(label_test[0], c)
-    return qc
 
 
 def exec_simulator(qc, n_shots=1024):
